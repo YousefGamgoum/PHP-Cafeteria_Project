40080fab48e6a7b4de7b2e3981750ab35aa66689
unknown <doniaghza1@gmail.com>
2025-03-09 22:06:37 +0200

    home

--- login.php
+++ login.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
-include('databaseconnection.php');
+include('database.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     try {
-        $stmt = $connection->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->execute([$username]);
 
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+        $stmt = $connection->prepare("SELECT id, username, password, role FROM users WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+        
         if ($user) {
             if (password_verify($password, $user['password'])) {
                 $_SESSION['user_id'] = $user['id'];
@@ -19,9 +22,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['role'] = $user['role'];
 
                 if ($user['role'] == 'admin') {
-                    header("Location: list.php");
+                    header("Location: ./admain.php");
                 } else {
-                    header("Location: ./myorder/myorder.php");
+                    header("Location: ./home.php");
 
 
                 }
